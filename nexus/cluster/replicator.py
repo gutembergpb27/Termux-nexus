f92e87886b9bfbd52a34cf466acdc8d544a334b4
Gutemberg Procópio Barbosa <gutembergpb27@gmail.com>
2026-07-22 14:06:21 -0300

    feat(cluster): record last replication timestamp

--- nexus/cluster/replicator.py
+++ nexus/cluster/replicator.py
@@ -1,6 +1,10 @@
+from datetime import datetime, timezone
+
+
 class ClusterReplicator:
     def __init__(self):
         self._sync_count = 0
+        self._last_sync_at = None
 
     def sync(self, leader, follower):
         if not leader.needs_sync(follower.export_state()["version"]):
@@ -10,8 +14,12 @@ class ClusterReplicator:
 
         if synchronized:
             self._sync_count += 1
+            self._last_sync_at = datetime.now(timezone.utc)
 
         return synchronized
 
     def sync_count(self):
         return self._sync_count
+
+    def last_sync_at(self):
+        return self._last_sync_at
